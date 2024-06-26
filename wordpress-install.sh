b793f183370e772da89449888e90c76587be95d6
saxtonator <aaron@saxtons.org.uk>
2024-06-26 23:46:41 +0100

    Update wordpress-install.sh
    Removed /var/www/html/wordpress reference. Folder doesn't exist

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -11,9 +11,9 @@ sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
 sudo mysql -e "CREATE USER wpuser@localhost identified by 'theweatherisnice'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo cd /var/www/html/wordpress
+sudo cd /var/www/html/
 sudo wget https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
 sudo chmod 640 wp-config.php 
-sudo chown -R www-data:www-data /var/www/html/wordpress
+sudo chown -R www-data:www-data /var/www/html/
 # sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
