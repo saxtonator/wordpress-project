a076d50bc73c2d073b66ffc4bf744b9a2ca957ce
saxtonator <aaron@saxtons.org.uk>
2024-06-26 13:12:05 +0100

    Update wordpress-install.sh
    added final database bash automation

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-sudo cd /var/www/html
+sudo cd /var/www/
+sudo rm -rf html
 sudo apt -y install unzip
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
 sudo rm latest.zip
-sudo mariadb -u root
-# create database wordpress;
-# create user wpuser@localhost identified by 'your-password';
-# grant all privileges on wordpress.* to wpuser@localhost;
-# flush privileges;
-# exit;
-# wget wp-config.php from s3bucket
+sudo mv wordpress html 
+# sudo mariadb -u root
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER wpuser@localhost identified by 'theweatherisnice'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
+sudo cd /var/www/html/wordpress
+sudo wget https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
 sudo chmod 640 wp-config.php 
-sudo cd /etc/nginx/conf.d/
+sudo chown -R www-data:www-data /var/www/html/wordpress
+# sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
