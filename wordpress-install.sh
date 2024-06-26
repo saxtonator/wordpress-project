e24ab9b2d4256bc381cbc501c1ad96e38f74230e
saxtonator <aaron@saxtons.org.uk>
2024-06-26 23:52:48 +0100

    Update wordpress-install.sh
    added all of the full paths to commands

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-sudo cd /var/www/
-sudo rm -rf html
+sudo rm -rf /var/www/html
 sudo apt -y install unzip
-sudo wget https://wordpress.org/latest.zip
-sudo unzip latest.zip
-sudo rm latest.zip
-sudo mv wordpress html 
+sudo wget -O /var/www/lastest.zip https://wordpress.org/latest.zip
+sudo unzip /var/www/latest.zip
+sudo rm /var/www/latest.zip
+sudo mv /var/www/wordpress /var/www/html 
 # sudo mariadb -u root
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
 sudo mysql -e "CREATE USER wpuser@localhost identified by 'theweatherisnice'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo cd /var/www/html/
-sudo wget https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
-sudo chmod 640 wp-config.php 
+sudo wget -O /var/www/html/wp-config.php https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
+sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/
 # sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
