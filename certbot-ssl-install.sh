c523c31f7816dda1428e6428e72bb3d5e7918259
saxtonator <aaron@saxtons.org.uk>
2024-07-31 11:31:48 +0100

    Create certbot-ssl-install.sh
    First file

---
+++ certbot-ssl-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update package list and install Certbot and Certbot Nginx plugin
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y certbot python3-certbot-nginx
+
+# Define your email
+EMAIL="aaron@saxtons.org.uk"
+
+# Use Certbot to obtain and install the SSL certificate
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL
+
+# Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
+sudo nginx -t && systemctl reload nginx
+
