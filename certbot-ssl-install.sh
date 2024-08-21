4becd9a82b0ffa9f56265282187e0a94d0cd3b76
danHavercroft <32074604+danHavercroft@users.noreply.github.com>
2024-08-21 11:39:57 +0100

    Updated domain to generate SSL for

--- certbot-ssl-install.sh
+++ certbot-ssl-install.sh
@@ -7,9 +7,10 @@ sudo apt install -y certbot python3-certbot-nginx
 
 # Define your email
 EMAIL="aaron@saxtons.org.uk"
+DOMAIN=wordpress.danhavercroft.co.uk
 
 # Use Certbot to obtain and install the SSL certificate
-sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
 
 # Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
 sudo nginx -t && systemctl reload nginx
