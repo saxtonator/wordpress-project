2bf04b85c765c39a20eb383aa03ed95302ac0c1e
danHavercroft <32074604+danHavercroft@users.noreply.github.com>
2024-08-21 13:29:34 +0100

    Added correct DB credentials and SALTS

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -19,13 +19,16 @@ sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$pas
 sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
 
-sudo wget -O /var/www/html/wp-config.php https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
+# sudo wget -O /var/www/html/wp-config.php https://saxtonator-bucket.s3.amazonaws.com/wp-config.php
+
+sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/
 
-sed -i "s/DB_PASSWORD/$password/g" /var/www/html/wp-config.php
-sed -i "s/DB_USER/$username/g" /var/www/html/wp-config.php
-sed -i "s/DB_NAME/$username/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
 
-# sudo cd /etc/nginx/conf.d/
-# sudo touch wordpress.conf pull from s3bucket
+SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+STRING='put your unique phrase here'
+printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
