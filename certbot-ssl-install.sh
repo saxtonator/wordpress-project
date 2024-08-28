aed325cc9deb116a86e17174fe7abcbdb1aae4ba
saxtonator <aaron@saxtons.org.uk>
2024-08-28 15:01:14 +0100

    Update certbot-ssl-install.sh

--- certbot-ssl-install.sh
+++ certbot-ssl-install.sh
@@ -6,8 +6,8 @@ sudo apt upgrade -y
 sudo apt install -y certbot python3-certbot-nginx
 
 # Define your email
-EMAIL="aaron@saxtons.org.uk"
-DOMAIN=wordpress.danhavercroft.co.uk
+EMAIL=REPLACE_EMAIL
+DOMAIN=REPLACE_DOMAIN
 
 # Use Certbot to obtain and install the SSL certificate
 sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
