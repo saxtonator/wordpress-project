f5d19042519e9c543c3e13c42bd81298e18c167f
saxtonator <aaron@saxtons.org.uk>
2025-01-07 23:15:23 +0000

    Update backend-install.sh
    Added database username and password creds setup.

--- backend-install.sh
+++ backend-install.sh
@@ -3,3 +3,16 @@ apt -y update && apt -y upgrade
 apt install mariadb-server mariadb-client -y
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
+
+# Generate password for use in WP DB
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
+
+# sudo mariadb -u root
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
